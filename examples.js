b4f26746a29fce008ee8eb3141980b721e7e68a0
aaaailuj <18719332+aaaailuj@users.noreply.github.com>
2024-10-28 21:20:44 +0100

    Add example snippets from class

---
+++ examples.js
@@ -0,0 +1,34 @@
+// let person1 = {
+//     name: "Julia",
+//     age: 25,
+//     isStudent: false
+// }
+
+// let person2 = {
+//     name: "Fouziya",
+//     age: 28,
+//     isStudent: true,
+//     friends: [person1]
+// }
+
+// let people = [person1, person2]
+
+// console.log(person1)
+// console.log(person2)
+// console.log(people)
+
+function isEven(num) {
+    if(num % 2 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// console.log(isEven(3))
+let age = 0
+for (let i = 1; i <= 10; i++) {
+    age += 1
+    console.log(age)
+}
+// console.log(age)
